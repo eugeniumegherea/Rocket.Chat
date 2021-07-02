fb7d81bbf73e476839dae31462a4b5c0414e5365
Evgeny Mitchell <evgeny.mitchell@techork.com>
2021-07-02 13:12:01 +0300

    fix: Add url filter for msg translations

--- app/autotranslate/server/autotranslate.js
+++ app/autotranslate/server/autotranslate.js
@@ -18,6 +18,7 @@ const Provider = Symbol('Provider');
 const TelRe = /^\+?[1-9]\d{1,14}$/;
 // eslint-disable-next-line no-control-regex
 const EmailRe = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+const UrlRe = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
 
 const translationCache = new LRU({
 	max: 500,
@@ -291,7 +292,7 @@ export class AutoTranslate {
 		let shouldTranslate = true;
 		const translationKey = hash(`${ targetLanguages[0] }-${ (message || {}).msg || '' }`);
 
-		if (message.msg && (TelRe.test(message.msg) || EmailRe.test(message.msg))) {
+		if (message.msg && (TelRe.test(message.msg) || EmailRe.test(message.msg) || UrlRe.text(message.msg))) {
 			shouldTranslate = false;
 		} else if (room.v._id === message.u._id) {
 			// we want to conditionally translate only client messages
